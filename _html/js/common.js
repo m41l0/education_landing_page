8bbf056408be62bb2cd121d91dae1f9340243c46
m41l0 <loginovmi@gmail.com>
2016-09-06 00:07:53 +1000

    tabs

--- _html/js/common.js
+++ _html/js/common.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
 	function wResize() {
-		$("header").css("height", $(window).height());
+		// $("header").css("height", $(window).height());
 	}
 	wResize();
 	$(window).resize(function () {
@@ -10,6 +10,11 @@ $(document).ready(function() {
 	$(".top_phone .wrapper .tab").click(function () {
 		$(".top_phone .wrapper .tab").removeClass("active").eq($(this).index()).addClass("active");
 		$(".top_phone .tab_item").hide().eq($(this).index()).fadeIn()
+	}).eq(0).addClass("active");
+
+	$(".tabs_header .wrapper .tab").click(function () {
+		$(".tabs_header .wrapper .tab").removeClass("active").eq($(this).index()).addClass("active");
+		$(".tabs_header .tab_item").hide().eq($(this).index()).fadeIn()
 	}).eq(0).addClass("active");
 
 	//Цели для Яндекс.Метрики и Google Analytics
